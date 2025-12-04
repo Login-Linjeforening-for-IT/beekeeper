b7c9d9ac4a28a36eba134309f79e59bfdc88a77d
0herm <0herm@protonmail.com>
2025-12-04 14:07:30 +0100

    fix import

--- api/src/handlers/traffic/post.ts
+++ api/src/handlers/traffic/post.ts
@@ -1,4 +1,4 @@
-import { FastifyReply, FastifyRequest } from 'fastify'
+import type { FastifyReply, FastifyRequest } from 'fastify'
 import run from '#db'
 import debug from '#utils/debug.ts'
 import config from '#constants'
