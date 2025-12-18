770a1bf5aa2c061c4949ce3d231ab3244f137791
0herm <0herm@protonmail.com>
2025-12-18 13:49:52 +0100

    fix name

--- api/src/handlers/favicon/getFavicon.ts
+++ api/src/handlers/favicon/getFavicon.ts
@@ -2,7 +2,7 @@ import type { FastifyReply, FastifyRequest } from 'fastify'
 import fs from 'fs'
 import path from 'path'
 
-export default async function getFaviconHandler(req: FastifyRequest, res: FastifyReply) {
+export default async function getFavicon(req: FastifyRequest, res: FastifyReply) {
     const faviconPath = path.join(process.cwd(), 'public', 'favicon.ico')
     const favicon = fs.readFileSync(faviconPath)
     res.type('image/x-icon').send(favicon)
